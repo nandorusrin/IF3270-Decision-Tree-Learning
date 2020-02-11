0cd0faf217e1284e9a6bd89f163b2e5875d58d56
Naufal <naufalkps@gmail.com>
2020-02-12 06:36:22 +0700

    ID3

--- Id3.py
+++ Id3.py
@@ -13,4 +13,9 @@ def gain(S,A):
     return entropy(S) - sum
 
 def ID3(examples, target_attribute, attributes):
+    #untuk semua examples, check positif semua atau gimana, kalo iya langsung return labelnya
+    #kalau atributnya kosong, return label dengan target_attribut mayoritas
+
+    #kalau engga : hitung entropy->hitung entropy tiap attribute, hitung gain
+    #dari gain, tentuin leaf node nya, kalau example kosong, return target_attribut mayoritas, kalau masih ada, rekursif panggil ID3 lagi
     pass
