7ae8bcc17823b3fd2badad2deafed3066bb3e924
ikraduya <ikraduya@gmail.com>
2020-02-13 14:30:06 +0700

    add accuracy to Id3

--- Id3.py
+++ Id3.py
@@ -5,7 +5,11 @@ import Tree
 
 class Id3:
   def __init__(self):
+    self._accuracy = 0.0
     super().__init__()
+
+  def get_accuracy(self):
+    return self._accuracy
 
   '''
   I.S	: List of label (target_values)
@@ -75,17 +79,71 @@ class Id3:
       
       return created_node
 
+  def _train_test_split(self, X, y, test_size=0.2):
+    sample_len = len(y)
+    cut_idx = int(sample_len*test_size)
+    X_test = pd.DataFrame(X.columns)
+    X_test = X.iloc[:cut_idx, :]
+    y_test = y.iloc[:cut_idx]
+    X = X.iloc[cut_idx:, :]
+    y = y.iloc[cut_idx:]
+    
+    return X, X_test, y, y_test
+
   def fit(self, X, y):
+    X, X_test, y, y_test = self._train_test_split(X, y)
+
     self._tree = self._construct_tree(X, y)
     self._tree.printTree()
+    self._accuracy = self._compute_accuracy(X_test, y_test)
 
     return self
 
+  def _predict(self, X):
+    ret = []
+    err = False
+    for i, row in X.iterrows():
+      node = self._tree
+      depth = 0
+      while (len(node.children) > 0): # node still inner node
+        if node.checkChildrenValueExist(row[node.value]):
+          node = node.gotoSpesificChildren(row[node.value])
+        else:
+          node = node.gotoMaxChildrenCount()
+
+        if node == None:
+          err = True
+          break
+        depth += 1
+      if err:
+        break
+      
+      ret.append(node.value)
+    return ret
+
+  def predict(self, X):
+    return self._predict(X)
+  
+  def _compute_accuracy(self, validation_sample, validation_target):
+    predictions = self._predict(validation_sample)
+
+    count_true = 0
+    for i in range(len(predictions)):
+      if predictions[i] == validation_target.iloc[i]:
+        count_true += 1
+    
+    return count_true / len(validation_target)
 
 dataTrain = pd.read_csv('play-tennis.csv')
 headers = list(dataTrain.columns[1:])
 feature_names, target_names = headers[:-1], headers[-1]
 
-print(dataTrain)
 clf = Id3()
 clf = clf.fit(dataTrain[feature_names], dataTrain[target_names])
+print('akurasi:', clf.get_accuracy())
+
+newData = pd.read_csv('play-tennis-predict.csv')
+headers_new = list(newData.columns[1:])
+feature_names_new, target_names_new = headers_new[:-1], headers_new[-1]
+
+print(clf.predict(newData[feature_names_new]))
